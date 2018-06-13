ab810d145276d85e392fe61b18c38fe0613b5645
marcolgl <marco-9-6-96@hotmail.it>
2018-06-13 21:14:13 +0200

    add_login_logout_part2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 	before_action :ensure_logged_in, only: [:logout]
 
+
+	# method called to logout, it previously check you are logged in
 	def logout
 		sign_out(@user)
 		redirect_to '/', notice: "Logout success"
