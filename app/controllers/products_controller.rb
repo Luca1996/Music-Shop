7eb253ff13a30f7d8eb364350d5612a27d7b0c04
Luca1996 <lucagiuffrida1996@gmail.com>
2018-06-13 11:17:40 +0200

    Added Products Controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+    
+    def new
+        # @product = Product.new
+    end
+    
+    def edit
+        # @product = Product.find(params[:id])    
+    end
+    
+    def index
+        # @products = Products.all()
+    end
+
+    def show
+        # @product = Product.find(params[:id])        
+    end
+    
+    def create
+        #@product = Product.create(product_params)
+        # to add all relations
+        #if @product.save
+        #   redirect_to root_path
+        # else
+            # render 'new'
+        # end
+    end
+    
+    def destroy
+        # @product = Product.find(params[:id])  
+        # @product.destroy
+        # redirect_to products_path
+    end
+
+
+    def update
+        # @product = Product.find(params[:id])        
+        # @produce.update(product_params)
+        # redirect_to product_path(product)
+    end
+    
+    
+    
+    private 
+        def product_params
+            #params.require()            
+        end
+        
+end
