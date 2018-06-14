b7d9991defdda4c6ec8d2d0e53ebb1bddf118723
marcolgl <marco-9-6-96@hotmail.it>
2018-06-14 19:36:43 +0200

    twitter_auth_not_working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,6 +259,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, 'FIRvnX8W52KT7OCFTOIo8PKaA', 'lWbVQrYcIha0LlSyC6XWML3sgGhZ7oY8bYac9wSV9A9LvR8pGs'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
