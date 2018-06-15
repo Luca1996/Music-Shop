918f54b959f30c99462ba2dbdfd9b0816bd44d68
Luca1996 <lucagiuffrida1996@gmail.com>
2018-06-15 15:39:43 +0200

    Card in index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_14_081628) do
+ActiveRecord::Schema.define(version: 2018_06_15_090114) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_06_14_081628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
