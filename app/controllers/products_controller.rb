b1f9c9400fd47e58627a98233e9f79356fb6bde7
Luca1996 <lucagiuffrida1996@gmail.com>
2018-06-16 14:46:19 +0200

    image upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,9 +18,10 @@ class ProductsController < ApplicationController
     
     def create
         @product = Product.new(product_params)
+        compress_image
         # to add all relations
         if @product.save!
-          redirect_to root_path
+          redirect_to product_path(@product)
         else
             render 'new'
         end
@@ -36,6 +37,8 @@ class ProductsController < ApplicationController
     def update
         @product = Product.find(params[:id])        
         @product.update(product_params)
+        compress_image
+        @product.save
         redirect_to products_path()
     end
     
@@ -44,7 +47,13 @@ class ProductsController < ApplicationController
     private 
         # we don't let the user set himself as admin obviously
         def product_params
-            params.require(:product).permit(:title,:brand,:model,:price,:quantity,:weight,:description)           
+            params.require(:product).permit(:title,:brand,:model,:price,:quantity,:weight,:description,:image)           
         end
-        
+
+        def compress_image
+            if !params[:product][:image].nil?
+                b64 = Base64.encode64(params[:product][:image].read)
+                @product.image = b64
+            end
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_090114) do
+ActiveRecord::Schema.define(version: 2018_06_16_120630) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_06_15_090114) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
