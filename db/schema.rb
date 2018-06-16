75a7d50bd7da2bb3b95a7b854dda3900a3fff0be
Luca1996 <lucagiuffrida1996@gmail.com>
2018-06-16 12:02:21 +0200

    modified some css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_14_181808) do
+ActiveRecord::Schema.define(version: 2018_06_15_090114) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,8 +59,7 @@ ActiveRecord::Schema.define(version: 2018_06_14_181808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
-    t.string "provider"
-    t.string "uid"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
