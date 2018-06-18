d6928e9619b387396939eb79ca7b7f6b50e6197c
marcolgl <marco-9-6-96@hotmail.it>
2018-06-18 19:39:06 +0200

    add_activities_controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'settings/show'
   patch 'settings/change_avatar'
   patch 'settings/change_password'
+  get 'activities/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
