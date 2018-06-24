fca771ad430606d563f1e047445e26205c7659e1
Luca1996 <lucagiuffrida1996@gmail.com>
2018-06-24 14:35:41 +0200

    to add key on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :product
+    validates :title, presence: true
+    validates_length_of :text, presence: true,:minimum => 1, :maximum => 500
 end
