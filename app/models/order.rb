1fac9805f7afa99c504d2bf12d9d0dc22fb1134d
Luca1996 <lucagiuffrida1996@gmail.com>
2018-07-02 18:18:43 +0200

    Added references lineitem - order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,11 @@
 class Order < ApplicationRecord
 	belongs_to :user
+	has_many :line_items
 	validates :address, presence: true, length: { minimum:5 }
 	validates :t_num, length: {maximum: 8}, format: { with: /\A[0-9]{8,12}/, message: "Insert a valid number" }
 	
 	validates :user_id, presence: true
-	
-	
-	
+
 	enum p_method: {
 		"PayPal"	         => 0,
 		"Cash on delivery"	 => 1
