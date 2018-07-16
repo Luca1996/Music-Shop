8fba1e82c934a4db4ea04197a9e9832f3fa3ae86
Luca1996 <lucagiuffrida1996@gmail.com>
2018-07-16 09:28:21 +0200

    last

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,5 +1,5 @@
 class Guitar < ApplicationRecord
-    validates :hand, format: { with: /\A(left)|(right)\Z/ }
+    validates :hand, format: { with: /\A(Left)\Z|\A(Right)\Z/ }
     validates :chords, :numericality => {:greater_than => 0}
     has_one :product, as: :instrum 	
 	accepts_nested_attributes_for :product
